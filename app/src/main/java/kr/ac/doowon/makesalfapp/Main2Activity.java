53b9bcda4f2bfdca5da590618ea0564dffe198a2
UMP45 <parksj1941@naver.com>
2025-07-14 16:00:10 +0900

    로그인

---
+++ app/src/main/java/kr/ac/doowon/makesalfapp/Main2Activity.java
@@ -0,0 +1,17 @@
+package kr.ac.doowon.makesalfapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Button btnSelectArea, btnMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
